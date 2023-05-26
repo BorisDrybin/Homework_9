2cd0e993c8cda94b50f5aae1ebf3a5f03cfa3bc1
Boris Drybin <makoh93@gmail.com>
2023-05-26 20:24:57 +0300

    second task added

--- Program.cs
+++ Program.cs
@@ -4,17 +4,50 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-PrintNumbers(n);
+// PrintNumbers(n);
 
-void PrintNumbers(int n)
+// void PrintNumbers(int n)
+// {
+//     if(n < 1)
+//     {
+//         return;
+//     }
+//     Console.Write($"{n} ");
+//     PrintNumbers(n - 1);
+// }
+
+
+//  Задайте значения M и N. Напишите программу, 
+//  которая найдёт сумму натуральных элементов 
+//  в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = ReadInt("Введите M: ");
+int n = ReadInt("Введите N: ");
+
+if(m > n)
 {
-    if(n < 1)
+    Console.WriteLine("Неверные данные");
+    return;
+}
+
+Console.Write(SumNumbers(m, n));
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int m, int n)
+{
+    if (m == n)
     {
-        return;
+        return m;
     }
-    Console.Write($"{n} ");
-    PrintNumbers(n - 1);
+    else return m + SumNumbers(m + 1, n);
 }
