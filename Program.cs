677553cf621701092b0f6041665222323dbe57a8
Boris Drybin <makoh93@gmail.com>
2023-05-26 20:40:09 +0300

    third task added

--- Program.cs
+++ Program.cs
@@ -20,22 +20,50 @@
 // }
 
 
+
 //  Задайте значения M и N. Напишите программу, 
 //  которая найдёт сумму натуральных элементов 
 //  в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// int m = ReadInt("Введите M: ");
+// int n = ReadInt("Введите N: ");
+
+// if(m > n)
+// {
+//     Console.WriteLine("Неверные данные");
+//     return;
+// }
+
+// Console.Write(SumNumbers(m, n));
+
+// int ReadInt(string text)
+// {
+//     Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int SumNumbers(int m, int n)
+// {
+//     if (m == n)
+//     {
+//         return m;
+//     }
+//     else return m + SumNumbers(m + 1, n);
+// }
+
+
+
+// Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 int m = ReadInt("Введите M: ");
 int n = ReadInt("Введите N: ");
 
-if(m > n)
-{
-    Console.WriteLine("Неверные данные");
-    return;
-}
-
-Console.Write(SumNumbers(m, n));
+Console.Write(AkkermanFunction(m, n));
 
 int ReadInt(string text)
 {
@@ -43,11 +71,15 @@ int ReadInt(string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int SumNumbers(int m, int n)
+int AkkermanFunction(int m, int n)
 {
-    if (m == n)
+    if (m == 0) 
     {
-        return m;
+        return n + 1;
     }
-    else return m + SumNumbers(m + 1, n);
+    else if (n == 0) 
+    {
+        return AkkermanFunction(m - 1,1);
+    }
+    else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
 }
